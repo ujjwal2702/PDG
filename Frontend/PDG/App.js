ba25529a5f4ea2aa75049bf9626e54a1dd5c21d5
Ujjwal <ujjwal2702@gmail.com>
2023-10-06 20:33:07 +0530

    setting up nativebaseui

--- Frontend/PDG/App.js
+++ Frontend/PDG/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NativeBaseProvider, Text, Box, Button, Input, Link } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>PDG</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center" backgroundColor={"black"}>
+        <Text color={"white"} fontSize={40} fontFamily={"Roboto"} >Welcome To PDG</Text>
+        <Input variant="underlined" marginBottom={3} width={80} placeholder="Username" />
+        <Input variant="underlined" marginBottom={3} width={80} placeholder="Password" />
+        <Button  marginTop={10} width={80}>Log In</Button>
+        <Link  marginTop={10} isExternal _text={{
+        color: "cyan.400"
+      }} mt={-0.5} _web={{
+        mb: -2
+      }} color={"white"}>Create an account</Link>
+      </Box>
+     
+
+    </NativeBaseProvider>
   );
 }
 
